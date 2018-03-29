9d4f9b842215b7e39e94e1c6cdf26c9791c0d9e5
Hashem Ramdan Mustafa <hashemramdan97@yahoo.com>
2018-03-29 16:44:47 +0200

    Matching game

---
+++ js/javascript.js
@@ -0,0 +1,69 @@
+var numberOfFaces = 5;
+    
+    var i = 0;
+       
+   var theLeftSide = document.getElementById("leftSide");
+
+   var theRightSide = document.getElementById("rightSide");
+
+   var theBody = document.getElementsByTagName("body")[0];
+
+   function generateFaces() {
+
+     "use strict";
+
+     while (theLeftSide.firstChild) { // While there is a child theNode.removeChild(theNode.firstChild);
+
+       theLeftSide.removeChild(theLeftSide.firstChild);
+     }       
+    
+    while (theRightSide.firstChild) { // While there is a child theNode.removeChild(theNode.firstChild);
+    theRightSide.removeChild(theRightSide.firstChild);}                
+
+
+
+     for (i = 0; i < numberOfFaces; ++ i) {
+
+       var top_position = Math.floor((Math.random() * 400));
+
+       var left_position = Math.floor((Math.random() * 400));
+
+       var smile_img = document.createElement("img");
+
+       smile_img.src = "http://home.cse.ust.hk/~rossiter/mooc/matching_game/smile.png"
+
+       smile_img.style.top = top_position + "px";
+
+       smile_img.style.left = left_position + "px";
+
+       theLeftSide.appendChild(smile_img);
+         
+    var leftSideImages = theLeftSide.cloneNode(true);
+
+     leftSideImages.removeChild(leftSideImages.lastChild);
+
+     theRightSide.appendChild(leftSideImages);
+         
+     }
+       
+ 
+     theLeftSide.lastChild.onclick = function nextLevel(event) {
+
+       event.stopPropagation();
+
+       numberOfFaces += 5;
+
+       generateFaces();
+
+     };
+
+   }
+     theBody.onclick = function gameOver() {
+
+       alert("Game Over!");
+
+       theBody.onclick = null;
+
+       theLeftSide.lastChild.onclick = null;
+
+     }; 
